e19f9026efbec5a7d30dda3cdac9748e31b2b8ab
Herob527 <szymon.wrzos@onet.pl>
2026-07-29 23:12:07 +0200

    Working on border

--- shared/src/commonMain/kotlin/com/example/transcripttest/App.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/App.kt
@@ -1,17 +1,30 @@
 package com.example.transcripttest
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -32,11 +45,36 @@ fun App(
     navController: NavHostController = rememberNavController()
 ) {
     Scaffold { innerPadding ->
-        Row(modifier = Modifier.padding(innerPadding)) {
-            Column {
+        Row(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .padding(innerPadding)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxHeight().width(100.dp)
+                    .drawBehind {
+                        val strokeWidth = 1* density
+                        val y = size.height - strokeWidth / 2
+
+                        drawLine(
+                            Color.Black,
+                            Offset(size.width, 0f),
+                            Offset(size.width, y),
+                            strokeWidth
+                        )
+                    }
+            ) {
                 Routes.entries.forEach {
-                    TextButton(onClick = { navController.navigate(it.name) }) {
-                        Text(it.title)
+                    TextButton(
+                        onClick = { navController.navigate(it.name) },
+                        modifier = Modifier
+                    ) {
+
+                        Text(
+                            it.title,
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Start
+                        )
                     }
                 }
             }
