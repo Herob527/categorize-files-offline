bd187f8a0b5b5fccbdade8b80bb771ca21f2f6c0
Herob527 <szymon.wrzos@onet.pl>
2026-07-29 22:54:46 +0200

    Remove redundant stuff

--- shared/src/commonMain/kotlin/com/example/transcripttest/App.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/App.kt
@@ -2,22 +2,19 @@ package com.example.transcripttest
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.transcripttest.screens.ExportScreen
 import com.example.transcripttest.screens.StartupScreen
@@ -34,12 +31,6 @@ enum class Routes(val title: String) {
 fun App(
     navController: NavHostController = rememberNavController()
 ) {
-
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    // Get the name of the current screen
-    val currentScreen = Routes.valueOf(
-        backStackEntry?.destination?.route ?: Routes.Startup.name
-    )
     Scaffold { innerPadding ->
         Row(modifier = Modifier.padding(innerPadding)) {
             Column {
