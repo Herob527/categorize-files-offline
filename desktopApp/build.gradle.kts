1373c3f40c47ab7387b38078fc657989be036da9
Herob527 <szymon.wrzos@onet.pl>
2026-08-03 18:06:08 +0200

    Remove redundant thing

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -14,11 +14,6 @@ dependencies {
     implementation(project(":shared"))
     implementation(compose.desktop.currentOs)
     implementation(libs.kotlinx.coroutinesSwing)
-
-    implementation(libs.koin.core)
-    implementation(libs.koin.compose)
-    implementation(libs.koin.compose.viewmodel)
-
 }
 
 compose.desktop {
