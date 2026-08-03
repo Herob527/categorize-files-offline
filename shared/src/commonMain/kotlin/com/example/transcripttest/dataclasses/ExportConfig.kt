338d8efe8d39b9d92c55c056fc6d6431d7bfcc72
Herob527 <szymon.wrzos@onet.pl>
2026-08-03 11:39:32 +0200

    Added fields

--- shared/src/commonMain/kotlin/com/example/transcripttest/dataclasses/ExportConfig.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/dataclasses/ExportConfig.kt
@@ -4,9 +4,35 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ExportConfig(
-    val shouldZip: Boolean,
-    val divideByCategory: Boolean,
-    val omitEmptyTexts: Boolean,
-    val exportText: String,
-    val textFormat: String,
+    val shouldZip: Boolean = false,
+    val uncategorizedAudiosName: String,
+    /**
+     * If false, prompt user (allow them to check if it should be remembered)
+     * If true, overwrite without question
+     */
+    val shouldOverwriteExport: Boolean = false,
+    val shouldExportTranscript: Boolean = true,
+    /**
+     * If true - every audio and texts will be in own directory according to category name
+     * If false - all audios and texts will be in one directory
+     *
+     * In both cases [audioDirectory] still applies
+     */
+    val divideByCategory: Boolean = true,
+    val omitEmptyTexts: Boolean = false,
+    /**
+     * If null, transcript and audios will be one dir
+     * If not null, audio will get own directory and audioPath in [transcriptFormat] will be prepended with path if it's not
+     */
+    val audioDirectory: String?,
+    /**
+     * Planned supported formats
+     *
+     * audioPath
+     * text
+     * categoryName
+     * categoryIndex
+     * duration
+     */
+    val transcriptFormat: String = "{audioPath}|{text}",
 )
