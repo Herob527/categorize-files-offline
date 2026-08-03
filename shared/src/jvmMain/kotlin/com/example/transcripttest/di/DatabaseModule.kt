de948c859d86270e67cd5b3862de67f7ba9b8e1d
Herob527 <szymon.wrzos@onet.pl>
2026-08-03 20:01:06 +0200

    Move path to getDatabaseFile

--- shared/src/jvmMain/kotlin/com/example/transcripttest/di/DatabaseModule.kt
+++ shared/src/jvmMain/kotlin/com/example/transcripttest/di/DatabaseModule.kt
@@ -6,12 +6,14 @@ import com.example.transcripttest.data.local.AppDatabase
 import com.example.transcripttest.dataclasses.Project
 import com.example.transcripttest.interfaces.Database
 import kotlinx.coroutines.Dispatchers
+import kotlin.io.path.Path
 
 class DatabaseModule : Database {
     override fun getDatabaseFile(currentProject: Project): AppDatabase {
+        val pathToDb = Path(currentProject.absoluteRootPath, currentProject.dbName).toString()
         val db =
             Room
-                .databaseBuilder<AppDatabase>(currentProject.getDbPath())
+                .databaseBuilder<AppDatabase>(pathToDb)
                 .addMigrations()
                 .setQueryCoroutineContext(Dispatchers.IO)
                 .setDriver(BundledSQLiteDriver())
