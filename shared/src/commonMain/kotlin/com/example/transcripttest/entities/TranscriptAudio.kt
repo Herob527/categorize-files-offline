fa2eb2ed855c5c3254958e6144d54018e2f51f7e
Herob527 <szymon.wrzos@onet.pl>
2026-08-03 10:50:44 +0200

    Remove embedded and add restrict

--- shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptAudio.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptAudio.kt
@@ -3,25 +3,27 @@ package com.example.transcripttest.entities
 import androidx.room3.Embedded
 import androidx.room3.Entity
 import androidx.room3.ForeignKey
+import androidx.room3.ForeignKey.Companion.RESTRICT
 
 @Entity(
     foreignKeys = [
         ForeignKey(
             entity = Audio::class,
             parentColumns = ["id"],
-            childColumns = ["audioId"]
+            childColumns = ["audioId"],
+            onUpdate = RESTRICT,
+            onDelete = RESTRICT
         ),
         ForeignKey(
             entity = Transcript::class,
             parentColumns = ["id"],
-            childColumns = ["transcriptId"]
+            childColumns = ["transcriptId"],
+            onUpdate = RESTRICT,
+            onDelete = RESTRICT
         )
     ]
 )
 data class TranscriptAudio(
     val audioId: Int,
     val transcriptId: Int,
-
-    @Embedded val audio: Audio,
-    @Embedded val transcript: Transcript
 )
--- shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptCategory.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptCategory.kt
@@ -3,25 +3,27 @@ package com.example.transcripttest.entities
 import androidx.room3.Embedded
 import androidx.room3.Entity
 import androidx.room3.ForeignKey
+import androidx.room3.ForeignKey.Companion.RESTRICT
 
 @Entity(
     foreignKeys = [
         ForeignKey(
             entity = Category::class,
             parentColumns = ["id"],
-            childColumns = ["categoryId"]
+            childColumns = ["categoryId"],
+            onUpdate = RESTRICT,
+            onDelete = RESTRICT
         ),
         ForeignKey(
             entity = Transcript::class,
             parentColumns = ["id"],
-            childColumns = ["transcriptId"]
+            childColumns = ["transcriptId"],
+            onUpdate = RESTRICT,
+            onDelete = RESTRICT
         )
     ]
 )
 data class TranscriptCategory(
     val categoryId: Int,
     val transcriptId: Int,
-
-    @Embedded val category: Category,
-    @Embedded val transcript: Transcript
 )
