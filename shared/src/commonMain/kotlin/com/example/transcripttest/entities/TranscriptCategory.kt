f3e1580d5ed1d660bfc85edcafbbc04dcc78ae15
Herob527 <szymon.wrzos@onet.pl>
2026-08-03 10:57:38 +0200

    Remove import

--- shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptCategory.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptCategory.kt
@@ -1,6 +1,5 @@
 package com.example.transcripttest.entities
 
-import androidx.room3.Embedded
 import androidx.room3.Entity
 import androidx.room3.ForeignKey
 import androidx.room3.ForeignKey.Companion.RESTRICT
