b75da3b4abda8cd992ec7dcf1f292a910e583b40
Herob527 <szymon.wrzos@onet.pl>
2026-08-03 10:31:10 +0200

    Added unique constraints to audio path and category name

--- shared/src/commonMain/kotlin/com/example/transcripttest/entities/Audio.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/entities/Audio.kt
@@ -1,10 +1,15 @@
 package com.example.transcripttest.entities
 
 import androidx.room3.Entity
+import androidx.room3.Index
 import androidx.room3.PrimaryKey
 import kotlin.time.Duration
 
-@Entity
+@Entity(
+    indices = [
+        Index("path", unique = true)
+    ]
+)
 data class Audio(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val path: String,
--- shared/src/commonMain/kotlin/com/example/transcripttest/entities/Category.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/entities/Category.kt
@@ -1,9 +1,14 @@
 package com.example.transcripttest.entities
 
 import androidx.room3.Entity
+import androidx.room3.Index
 import androidx.room3.PrimaryKey
 
-@Entity
+@Entity(
+    indices = [
+        Index("name", unique = true)
+    ]
+)
 data class Category(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String
