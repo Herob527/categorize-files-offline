1c9b31d321c6e0cb63005426c92a954e54773fe1
Herob527 <szymon.wrzos@onet.pl>
2026-08-03 10:57:49 +0200

    Remove import and add primary keys

--- shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptAudio.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/entities/TranscriptAudio.kt
@@ -1,11 +1,11 @@
 package com.example.transcripttest.entities
 
-import androidx.room3.Embedded
 import androidx.room3.Entity
 import androidx.room3.ForeignKey
 import androidx.room3.ForeignKey.Companion.RESTRICT
 
 @Entity(
+    primaryKeys = ["audioId", "transcriptId"],
     foreignKeys = [
         ForeignKey(
             entity = Audio::class,
