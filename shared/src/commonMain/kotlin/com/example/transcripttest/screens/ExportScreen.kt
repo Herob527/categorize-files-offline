af224aa6e59e7ef4652883e04924073df7936345
Herob527 <szymon.wrzos@onet.pl>
2026-08-17 10:08:24 +0200

    Remove redundant experiemntal

--- shared/src/commonMain/kotlin/com/example/transcripttest/screens/ExportScreen.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/screens/ExportScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExportScreen() {
     return Column {
