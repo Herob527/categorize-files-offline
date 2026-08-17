3093e81f6aeada5b74d9c49b2f3e336dedf1022d
Herob527 <szymon.wrzos@onet.pl>
2026-08-17 16:17:53 +0200

    Add displaying current project

--- shared/src/commonMain/kotlin/com/example/transcripttest/screens/DashboardScreen.kt
+++ shared/src/commonMain/kotlin/com/example/transcripttest/screens/DashboardScreen.kt
@@ -3,10 +3,17 @@ package com.example.transcripttest.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.example.transcripttest.ProjectListViewModel
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun DashboardScreen() {
+fun DashboardScreen(
+    viewModel: ProjectListViewModel = koinViewModel(),
+) {
+    val state by viewModel.projectListState.collectAsState()
     return Column {
-        Text("Dashboard")
+        Text("Dashboard ${state.currentProject?.absoluteRootPath}" )
     }
 }
